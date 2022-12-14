9de4e07b61e173361e07f836e5aa0380fef5e51a
raghavk0077 <raghavkhanna1301@gmail.com>
2022-12-14 20:28:39 +0530

    Add code for fibonacci series using recursion

---
+++ Recursion/fibonacciSeries.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class fibonacciSeries {
+
+    static int fib(int n){
+        if(n <= 1)
+            return n;
+        
+        return fib(n-1) + fib(n-2);
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        sc.close();
+
+        System.out.println("Result = " + fib(n));
+    }
+}
+/*
+ Time complexity = O(2^n) exponential
+ Space complexity = O(2^n) exponential
+ */
